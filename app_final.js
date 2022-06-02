17ef984fdc33120f42e2ddc11bce3b2ab1312a5b
davifelinto <40410923+davifelinto@users.noreply.github.com>
2022-06-02 19:19:02 -0300

    App sem as funcionalidades da ultima entrega
    Rodando todas as funções que deveria ter rodado semana passada.

--- app_final.js
+++ app_final.js
@@ -3,12 +3,14 @@
 // Alunos:   Davi Hugo Sateles Felinto – UC 21105738
 //          Jackes Ridan da Silva Guedes Júnior – UC19106906
 
-// consts para leitura e registro de dados no banco, chamado de listateste aqui
+// Imports
 const fs = require("fs");
 const { parse } = require("csv-parse");
+const fastcsv = require("fast-csv")
 const mysql = require('mysql');
 const slug = require('slug')
-const { VARCHAR, DATE, INT,  } = require('mysql/lib/protocol/constants/types');
+const writableStream = fs.createWriteStream("saida.csv");
+
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -17,30 +19,12 @@ const con = mysql.createConnection({
   multipleStatements: true
 });
 
-// consts para criacao do arquivo de saida
-const { stringify } = require("csv-stringify");
-const filename = "saida.csv";
-const writableStream = fs.createWriteStream(filename);
-const columns = [
-  "nome_fantasia",
-  "slug",
-  "inicio_atividades",
-  "porte_empresa",
-  "nome_cidade",
-  "sigla_uf",
-  "populacao_cidade",
-  "latitude_cidade",
-  "longitude_cidade",
-];
-const stringifier = stringify({ header: true, columns: columns });
-
-
-// um unico connect para fazer todas as operacoes
+// Um unico connect para fazer todas as operacoes
 con.connect(async function(err) {
     if (err) throw err;
     console.log("Connected!");
 
-        // Criacao do banco;
+        // Criar banco - ok
         await new Promise((resolve, reject) => {
             var sql = "DROP DATABASE IF EXISTS ListaTeste;\
                         CREATE DATABASE ListaTeste;\
@@ -74,6 +58,7 @@ con.connect(async function(err) {
                             primary key (id),\
                             foreign key (cidade_id)\
                                 references cidade (id));";
+            
             con.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log("Table empresa Database reseted");
@@ -81,9 +66,10 @@ con.connect(async function(err) {
             });
         })
 
-    // primeira operacao: ler e inseir uf - check
+    // Ler e inseir uf - ok
     await new Promise((resolve, reject) => {
         var sql = "INSERT INTO listateste.uf (id, sigla) VALUES (?, ?)"
+
         fs.createReadStream("res/uf.csv")
         .pipe(parse({ delimiter: ",", from_line: 2 }))
         .on("data", function (row) {
@@ -95,9 +81,11 @@ con.connect(async function(err) {
         });
     });
 
-    // segunda operacao: ler e inserir cidade_siafi - check
+    // Ler e inserir cidade_siafi - ok
     await new Promise((resolve, reject) => {
-        var sql = "INSERT INTO listateste.cidade (uf_id, nome, latitude, longitude, cod_ibge, cod_siafi) VALUES (?, ?, ?, ?, ?, ?)"
+        var sql = "INSERT INTO listateste.cidade (uf_id, nome, latitude, longitude, cod_ibge, cod_siafi)\
+                    VALUES (?, ?, ?, ?, ?, ?)"
+
         fs.createReadStream("res/cidade_siafi.csv")
         .pipe(parse({ delimiter: ",", from_line: 2 }))
         .on("data", function (row) {
@@ -109,10 +97,11 @@ con.connect(async function(err) {
         });
     })
 
-    // Criacao de coluna auxiliar no banco - check
+    // Criar de coluna auxiliar no banco - ok
     await new Promise((resolve, reject) => {
         var sql = "ALTER TABLE listateste.empresa ADD municipio_siafi VARCHAR(255);\
                     SET SQL_SAFE_UPDATES = 0;";
+
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Table empresa altered");
@@ -120,9 +109,11 @@ con.connect(async function(err) {
         });
     })
 
-    // primeira operacao: ler e inseir empresa - check
+    //Ler e inseir empresas_bahia - ok
     await new Promise((resolve, reject) => {
-        var sql = "INSERT INTO listateste.empresa (slug, nome_fantasia, dt_inicioatividade, cnae_fiscal, cep, porte, municipio_siafi) VALUES (?, ?, ?, ? , ?, ?, ?)"
+        var sql = "INSERT INTO listateste.empresa (slug, nome_fantasia, dt_inicioatividade, cnae_fiscal, cep, porte, municipio_siafi)\
+                     VALUES (?, ?, ?, ? , ?, ?, ?)"
+        
         fs.createReadStream("res/empresas_bahia.csv")
         .pipe(parse({ delimiter: ",", from_line: 2 }))
         .on("data", function (row) {
@@ -134,13 +125,13 @@ con.connect(async function(err) {
         });
     })
 
-    // quarta operacao: dar update nos valores faltantes arquivo cidade_populacao - check
+    // Update nos valores faltantes. Arquivo: cidade_populacao - ok
     await new Promise((resolve, reject) => {
-        //params = Number(String(row[0]).slice(0,2)),row[1],row[3],row[0]
         var sql = "UPDATE listateste.uf SET nome_uf = ? WHERE id = ?;\
                     UPDATE listateste.cidade SET populacao = ? WHERE cod_ibge = ?;\
                     UPDATE listateste.empresa SET cidade_id = (SELECT id FROM listateste.cidade WHERE cod_ibge = ?)\
                         WHERE municipio_siafi = (SELECT cod_siafi FROM listateste.cidade WHERE cod_ibge = ?);"
+
         fs.createReadStream("res/cidade_populacao.csv")
         .pipe(parse({ delimiter: ",", from_line: 2 }))
         .on("data", function (row) {
@@ -154,7 +145,7 @@ con.connect(async function(err) {
         });
     })
 
-    // Drop da coluna auxiliar criada na tabela do banco
+    // Drop da coluna auxiliar criada na tabela do banco - ok
     await new Promise((resolve, reject) => {
         var sql = "ALTER TABLE listateste.empresa DROP COLUMN municipio_siafi;\
                     SET SQL_SAFE_UPDATES = 1;";
@@ -165,10 +156,9 @@ con.connect(async function(err) {
         });
     })
 
-    // Gravação de arquivo csv, nao ta pegando nada da query por algum motivo, nao deu pra resolver a tempo
-    // O problema provavelmente é o método de gravação que tá todo errado.
+    // Gravacao de arquivo csv - ok
     await new Promise((resolve, reject) => {
-        
+
         sql = "SELECT empresa.nome_fantasia, empresa.slug, empresa.dt_inicioatividade as inicio_atividades, empresa.porte as porte_empresa, cidade.nome as nome_cidade, uf.sigla as sigla_uf, cidade.populacao as populacao_cidade, cidade.latitude as latitude_cidade, cidade.longitude as longitude_cidade\
         FROM uf\
         JOIN cidade\
@@ -176,17 +166,24 @@ con.connect(async function(err) {
         RIGHT JOIN empresa\
             ON cidade.id = empresa.cidade_id"
 
-        con.each(query(sql, (err, row) => {
-            if (error) {
-              return console.log(error.message);
-            }
-            stringifier.write(row);
-            resolve();
-          }));
-          stringifier.pipe(writableStream);
-          console.log("Finished writing data");
+        con.query(sql, function (err, data) {
+        if (err) throw err;
+    
+        //JSON
+        const jsonData = JSON.parse(JSON.stringify(data));
+        console.log("jsonData", jsonData);
+    
+        //csv
+        fastcsv
+            .write(jsonData, { headers: true })
+            .on("finish", function () {
+            console.log("Finished writing data!");
+            })
+            .pipe(writableStream);
+        });
     })
 
+    // Desconeta do banco - ok
     await new Promise((resolve, reject) => {
         con.end();
         console.log("Disonnected!");
